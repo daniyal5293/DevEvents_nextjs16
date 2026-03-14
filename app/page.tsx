f7cae0bc953b45a88d6fe5de74a6707d332d56fe
daniyal5293 <daniyalkhan5293@gmail.com>
2026-03-14 18:10:07 +0500

    app/page.tsx fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
 
 
-const BASE_URL = process.env.BASE_URL;
+// const BASE_URL = process.env.BASE_URL;q
 
 const Home = async () => {
   'use cache'
   cacheLife('hours');
-  const response = await fetch(`${BASE_URL}/api/events`);
+  const response = await fetch(`${process.env.BASE_URL}/api/events`);
   const { events } = await response.json();
 
-  console.log("Events:");
+ 
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
